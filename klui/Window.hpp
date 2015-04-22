eed6c08180710b9121f322d11cfca18f2c30c90e
Kyung yul Kevin Lim <thekoalaz@gmail.com>
2015-04-21 21:37:10 -0700

    Filling in some header class prototypes.

--- klui/Window.hpp
+++ klui/Window.hpp
@@ -1,8 +1,36 @@
 #pragma once
-class Window
+#include "klui.hpp"
+
+namespace klui
+{ 
+
+/** Global variables **/
+
+/** Class Prototypes **/
+class Window;
+
+/** Class Definitions **/
+/** Window Class **/
+/** Abstract class that defines the basic properties of an UI element
+ *
+**/
+class Window : public UIElement
 {
 public:
-    Window();
-    ~Window();
+    /* Constructors */
+    Window(int width, int height) : UIElement(width, height)
+    { setName(DEFAULT_WINDOWNAME); init(); }
+    Window(int width, int height, std::string name) : UIElement(width, height, name) { init(); }
+
+    void init();
+    void draw();
+    void reshape(int w, int h);
+    
+private:
+    static void displayFuncWrapper();
+    static void reshapeFuncWrapper(int w, int h);
+
+    bool _init;
 };
 
+};
--- klui/klui.hpp
+++ klui/klui.hpp
@@ -1,1 +1,107 @@
 #include <SFML/Graphics.hpp>
+
+namespace klui
+{
+
+/** Global variables **/
+
+/** Class Prototypes **/
+class Manager;
+class UIElement;
+extern class Window;
+extern class Panel;
+extern class Button;
+
+/** Class Definitions **/
+
+/** Manager Class **/
+/** Manages and exposes all the UI elements.
+ * There should only ever be one manager.
+**/
+class Manager
+{
+public:
+    /* Constructors */
+    Manager() { }
+
+    void init(int argc, char* argv[]);
+    void display();
+    void drawElements();
+    void setShaders(std::string vertfile, std::string fragfile);
+    void deleteShaders();
+
+    Window & createWindow(int width, int height);
+    Window & createWindow(int width, int height, std::string name);
+    Window & createWindow(int width, int height, int xpos, int ypos, std::string name);
+
+    Panel & createPanel(Window & window, int width, int height, std::string name);
+    Panel & createPanel(Window & window, int width, int height, int xpos, int ypos, std::string name);
+
+    Button & createButton(Panel & panel, int width, int height, int xpos, int ypos, std::string name);
+
+private:
+    std::vector<Window *> _windows;
+    std::vector<UIElement *> _elements;
+    GLuint v, f, p;
+};
+
+/** UIElement Class **/
+/** Abstract class that defines the basic properties of an UI element
+ *
+**/
+class UIElement
+{
+public:
+    /* Constructors */
+    UIElement(int width, int height)
+        : _xpos(DEFAULT_XPOS), _ypos(DEFAULT_YPOS), _width(width), _height(height), _name(NULL)
+        { init(); }
+    UIElement(int width, int height, std::string name)
+        : _xpos(DEFAULT_XPOS), _ypos(DEFAULT_YPOS), _width(width), _height(height), _name(name)
+        { init(); }
+    UIElement(int xpos, int ypos, int width, int height, std::string name)
+        : _xpos(xpos), _ypos(ypos), _width(width), _height(height), _name(name)
+        { init(); }
+
+    
+    virtual void init();
+    virtual void draw() = 0;
+    
+
+    /* getters */
+    int getXPos() { return _xpos; } const
+    int getYPos() { return _ypos; } const
+    int getWidth() { return _width; } const 
+    int getHeight() { return _height; } const
+    std::string getName() { return _name; } const
+    int getId() { return _UIElemId; } const
+
+    std::vector<UIElement *> & getChildren() { return _children; } const
+
+
+    /* setters */
+    void setXPos(int xpos) { _xpos=xpos; }
+    void setYPos(int ypos) { _ypos=ypos; }
+    void setWidth(int width) { _width=width; }
+    void setHeight(int height) { _height=height; }
+    void setName(std::string name) { _name=name; }
+
+    void addChildren(UIElement * elem) { _children.push_back(elem); }
+
+    /* Single line functions */
+    unsigned int nextId() { return NEXTID++; }
+
+private:
+    int _xpos, _ypos,
+        _width, _height;
+    std::string _name;
+    bool _ortho;
+    float _opacity;
+    unsigned int _UIElemId;
+    std::vector<UIElement *> _children;
+
+    
+    static int NEXTID;
+};
+
+}
